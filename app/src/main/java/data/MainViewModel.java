f01776376cbcc27678856f3a2f39d81906b7f042
wenjie <jian0076@algonquinlive.com>
2022-09-19 16:25:17 -0400

    adding compound buttons

---
+++ app/src/main/java/data/MainViewModel.java
@@ -0,0 +1,9 @@
+package data;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+//    public String editString;
+    public MutableLiveData<String> editString = new MutableLiveData<>();
+}
