695957928a4198035e0aca10707805276e849200
wenjie <jian0076@algonquinlive.com>
2022-09-20 20:35:41 -0400

    Finished week 2

--- app/src/main/java/algonquin/cst2335/jian0076/data/MainViewModel.java
+++ app/src/main/java/algonquin/cst2335/jian0076/data/MainViewModel.java
@@ -1,9 +1,10 @@
-package data;
+package algonquin.cst2335.jian0076.data;
 
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
-//    public String editString;
     public MutableLiveData<String> editString = new MutableLiveData<>();
+    public MutableLiveData<Boolean> isSelected =new MutableLiveData<>(false);
 }
+
