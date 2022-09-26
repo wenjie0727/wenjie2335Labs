6b0978c3ee5cfc0e0accb5b56f7fc2e9f6497fd2
wenjie <jian0076@algonquinlive.com>
2022-09-26 14:43:27 -0400

    Finished Phone Version of Lab3

--- app/src/main/java/algonquin/cst2335/jian0076/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/jian0076/ui/MainActivity.java
@@ -1,6 +1,11 @@
 package algonquin.cst2335.jian0076.ui;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,13 +15,30 @@ import algonquin.cst2335.jian0076.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding variableBinding;
 
-
+    ImageView imgView;
+    Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(variableBinding.getRoot());}}
+        setContentView(variableBinding.getRoot());
+        imgView = variableBinding.flagView;
+        sw = variableBinding.spinSwitch;
+
+        sw.setOnCheckedChangeListener((btn,isChecked) ->{
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+                imgView.startAnimation(rotate);
+            }
+         else {
+            imgView.clearAnimation();
+        }
+});
+    }}
 
 
