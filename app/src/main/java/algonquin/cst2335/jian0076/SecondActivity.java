fefd72693e56c2a2c2fd17880f2c2de7b648a91e
wenjie <jian0076@algonquinlive.com>
2022-10-11 14:55:58 -0400

    week4-1 version3

--- app/src/main/java/algonquin/cst2335/jian0076/SecondActivity.java
+++ app/src/main/java/algonquin/cst2335/jian0076/SecondActivity.java
@@ -1,8 +1,10 @@
 package algonquin.cst2335.jian0076;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -13,10 +15,46 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import algonquin.cst2335.jian0076.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
     ActivitySecondBinding binding;
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        binding = ActivitySecondBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        File file = new File(getFilesDir(), "Picture.png");
+        if (file.exists()) {
+            String path = file.getAbsolutePath();
+            Bitmap thumbnail = BitmapFactory.decodeFile(path +"/Picture.png");
+            binding.profileImage.setImageBitmap(thumbnail);
+        }
+        Intent fromPrevious = getIntent();
+        String emailAddress = fromPrevious.getStringExtra("EmailAddress");
+        binding.textView3.setText("Welcome " + emailAddress);
+
+        binding.button.setOnClickListener(click -> {
+            Intent call = new Intent(Intent.ACTION_DIAL);
+            String phoneNumber = binding.editTextPhone.getText().toString();
+            call.setData(Uri.parse("tel:" + phoneNumber));
+            startActivity(call);
+        })
+        ;
+        binding.button2.setOnClickListener(click ->
+        {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            cameraResult.launch(cameraIntent);
+        });
+    }
+
+
     ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
@@ -27,31 +65,18 @@ public class SecondActivity extends AppCompatActivity {
                         Intent data = result.getData();
                         Bitmap thumbnail = data.getParcelableExtra("data");
                         binding.profileImage.setImageBitmap(thumbnail);
-                    }
+                        FileOutputStream fOut = null;
+                        try {
+                            fOut = openFileOutput("Picture.png", Context.MODE_PRIVATE);
+                            thumbnail.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+                            fOut.flush();
+                            fOut.close();
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }}};
+
+
+                    });
                 }
-            });
-
-                @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = ActivitySecondBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        Intent fromPrevious = getIntent();
-        String emailAddress= fromPrevious.getStringExtra("EmailAddress");
-        binding.textView3.setText("Welcome " + emailAddress);
-
-        binding.button.setOnClickListener( click ->{
-            Intent call = new Intent(Intent.ACTION_DIAL);
-            String phoneNumber = binding.editTextPhone.getText().toString();
-            call.setData(Uri.parse("tel:" + phoneNumber));
-            startActivity(call);
-                }
-                );
-        binding.button2.setOnClickListener(click ->
-        {
-            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            cameraResult.launch(cameraIntent);
-        });
-    }
-}
